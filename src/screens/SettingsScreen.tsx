97076ef9baa956c3d6ee50390d4a7244bf68d26d
Sterling Muller <sterlingmuller93@gmail.com>
2024-04-18 19:12:08 -0500

    navigation headers

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -5,18 +5,16 @@ import settingsStyle from '@styles/settings';
 import BackupAndSync from '@src/settings/components/BackupAndSync';
 import Theme from '@src/settings/components/Theme';
 import About from '@src/settings/components/About';
-import Header from '@src/settings/components/Header';
 
 const SettingsScreen = () => (
-    <View style={settingsStyle.container}>
-      <Header />
-      <View style={settingsStyle.content}>
-        <BackupAndSync />
-        <Theme />
-        <About />
-      </View>
-      <Text style={settingsStyle.version}>Version 0.0.0</Text>
+  <View style={settingsStyle.container}>
+    <View style={settingsStyle.content}>
+      <BackupAndSync />
+      <Theme />
+      <About />
     </View>
+    <Text style={settingsStyle.version}>Version 0.0.0</Text>
+  </View>
 );
 
 export default SettingsScreen;
--- src/styles/header.ts
+++ src/styles/header.ts
@@ -1,16 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 const headerStyles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    height: 115,
-    paddingTop: 45,
-    alignItems: 'center',
-    backgroundColor: 'coral',
-    zIndex: 10,
-    gap: 20,
-  },
+  headerStyle: { backgroundColor: 'coral' },
+  headerTitleStyle: { fontSize: 24 },
 });
 
 export default headerStyles;
