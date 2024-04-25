1c8b9e028375d39bbb9427b6cc3e31286414d312
Sterling Muller <sterlingmuller93@gmail.com>
2024-04-25 17:03:05 -0500

    disable save buttons when no text

--- src/lyrics/components/LyricsSheet.tsx
+++ src/lyrics/components/LyricsSheet.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput } from 'react-native';
 
 import lyricSheetStyles from '@src/styles/lyricsSheet';
@@ -8,21 +8,32 @@ import MetronomeIcon from '@src/icons/MetronomeIcon';
 import ShareIcon from '@src/icons/ShareIcon';
 import SaveAndCancelButtons from '@src/common/components/SaveAndCancelButtons';
 
-const LyricsSheet = () => (
-  <View style={lyricSheetStyles.container}>
-    <View style={lyricSheetStyles.options}>
-      <EditIcon />
-      <ChordsIcon />
-      <MetronomeIcon />
-      <ShareIcon />
+const LyricsSheet = () => {
+  const [lyrics, setLyrics] = useState<string>('');
+  const disabled: boolean = !lyrics;
+
+  return (
+    <View style={lyricSheetStyles.container}>
+      <View style={lyricSheetStyles.options}>
+        <EditIcon />
+        <ChordsIcon />
+        <MetronomeIcon />
+        <ShareIcon />
+      </View>
+      <TextInput
+        style={lyricSheetStyles.textContainer}
+        value={lyrics}
+        onChangeText={(text: string) => setLyrics(text)}
+        placeholder="Add lyrics..."
+        textAlignVertical="top"
+      />
+      <SaveAndCancelButtons
+        onPress={() => null}
+        onExitPress={() => null}
+        disabled={disabled}
+      />
     </View>
-    <TextInput
-      style={lyricSheetStyles.textContainer}
-      placeholder="Add lyrics..."
-      textAlignVertical="top"
-    />
-    <SaveAndCancelButtons />
-  </View>
-);
+  );
+};
 
 export default LyricsSheet;
