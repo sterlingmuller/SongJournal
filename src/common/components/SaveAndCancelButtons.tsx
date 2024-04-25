894738226c62035799bf7b70d6968646d99b70fd
Sterling Muller <sterlingmuller93@gmail.com>
2024-04-25 17:03:05 -0500

    disable save buttons when no text

--- src/common/components/SaveAndCancelButtons.tsx
+++ src/common/components/SaveAndCancelButtons.tsx
@@ -3,21 +3,29 @@ import React from 'react';
 import { View, Button } from 'react-native';
 
 interface Props {
-  // buttonsStyle;
-  // onPress;
-  // onExitPress;
+  onPress: () => void;
+  onExitPress: () => void;
+  disabled: boolean;
+  buttonsStyle?;
 }
 
-const SaveAndCancelButtons = (props: Props) => (
-  // const [buttonsStyle, onPress, onExitPress] = props;
+const SaveAndCancelButtons = (props: Props) => {
+  const { buttonsStyle, onPress, onExitPress, disabled } = props;
 
-  <View style={saveAndCancelButtonsStyle.buttons}>
-    <View style={saveAndCancelButtonsStyle.button}>
-      <Button title="Save" onPress={() => null} color="#81C2F1" />
+  return (
+    <View style={saveAndCancelButtonsStyle.buttons}>
+      <View style={saveAndCancelButtonsStyle.button}>
+        <Button
+          title="Save"
+          onPress={onPress}
+          color="#81C2F1"
+          disabled={disabled}
+        />
+      </View>
+      <View style={saveAndCancelButtonsStyle.button}>
+        <Button title="Cancel" color="#D6D6D6" onPress={onExitPress} />
+      </View>
     </View>
-    <View style={saveAndCancelButtonsStyle.button}>
-      <Button title="Cancel" color="#D6D6D6" onPress={() => null} />
-    </View>
-  </View>
-);
+  );
+};
 export default SaveAndCancelButtons;
--- src/songFolder/components/NotesModal.tsx
+++ src/songFolder/components/NotesModal.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
-import {
-  View, Text, Modal, TouchableOpacity, TextInput,
-} from 'react-native';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { View, Text, Modal, TouchableOpacity, TextInput } from 'react-native';
 
 import takeNotesModalStyle from '@styles/takeNotesModal';
-import { RootStackParamList } from '@src/common/types';
 import SaveAndCancelButtons from '@src/common/components/SaveAndCancelButtons';
 
 interface Props {
@@ -14,10 +10,10 @@ interface Props {
 }
 
 const NotesModal = ({ isNotesModalOpen, setIsNotesModalOpen }: Props) => {
-  const { navigate } = useNavigation<NavigationProp<RootStackParamList>>();
   const [notes, setNotes] = useState<string>('');
 
   const onExitPress = () => setIsNotesModalOpen('');
+  const disabled: boolean = !notes;
 
   return (
     <Modal transparent visible={!!isNotesModalOpen}>
@@ -27,26 +23,24 @@ const NotesModal = ({ isNotesModalOpen, setIsNotesModalOpen }: Props) => {
         onPress={onExitPress}
       >
         <View style={takeNotesModalStyle.container}>
-          <Text style={takeNotesModalStyle.title}>{isNotesModalOpen} Notes</Text>
+          <Text style={takeNotesModalStyle.title}>
+            {isNotesModalOpen} Notes
+          </Text>
           <View style={takeNotesModalStyle.textbox}>
             <TextInput
               style={takeNotesModalStyle.input}
               placeholder="Add notes for the current take..."
               value={notes}
-              onChangeText={(newNotes) => setNotes(newNotes)}
+              onChangeText={(text: string) => setNotes(text)}
               multiline={true}
               textAlignVertical="top"
             />
           </View>
-          {/* <View style={takeNotesModalStyle.buttons}>
-            <View style={takeNotesModalStyle.button}>
-              <Button title="Save" color="#81C2F1" onPress={() => navigate('CurrentSongFolder')} />
-            </View>
-            <View style={takeNotesModalStyle.button}>
-              <Button title="Cancel" color="#D6D6D6" onPress={onExitPress} />
-            </View>
-          </View> */}
-          <SaveAndCancelButtons />
+          <SaveAndCancelButtons
+            onPress={() => null}
+            onExitPress={() => setIsNotesModalOpen('')}
+            disabled={disabled}
+          />
         </View>
       </TouchableOpacity>
     </Modal>
