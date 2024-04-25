f41f4aea9eff1699372e7d854dcbec6fb7b52007
Sterling Muller <sterlingmuller93@gmail.com>
2024-04-24 19:23:24 -0500

    fix constant name for color themes

--- src/settings/components/About.tsx
+++ src/settings/components/About.tsx
@@ -4,21 +4,21 @@ import { Text, View } from 'react-native';
 import settingsStyle from '@styles/settings';
 
 const About = () => (
+  <View>
+    <Text style={settingsStyle.sectionTitle}>About</Text>
     <View>
-      <Text style={settingsStyle.sectionTitle}>About</Text>
-      <View>
-        <Text style={settingsStyle.about}>
-          This app was made by Sterling Muller. I don’t what a Terms of Service
-          is so we’re probably fine. I hope you get a lot of good use out of my
-          app, make killer songs, and spread the word to friends.
-        </Text>
-        <Text style={settingsStyle.aboutSignature}>Cheers,</Text>
-        <Text style={settingsStyle.about}>Sterling</Text>
-        <Text style={settingsStyle.aboutSignature}>
-          sterlingmuller93@gmail.com if you want to fight
-        </Text>
-      </View>
+      <Text style={settingsStyle.about}>
+        This app was made by Sterling Muller. I don’t know what a Terms of
+        Service is so we’re probably fine. I hope you get a lot of good use out
+        of my app, make killer songs, and spread the word to friends.
+      </Text>
+      <Text style={settingsStyle.aboutSignature}>Cheers,</Text>
+      <Text style={settingsStyle.about}>Sterling</Text>
+      <Text style={settingsStyle.aboutSignature}>
+        sterlingmuller93@gmail.com if you want to fight
+      </Text>
     </View>
+  </View>
 );
 
 export default About;
