8e43755da676e302db755b9276c57180abe1efe7
Sterling Muller <sterlingmuller93@gmail.com>
2024-05-01 01:54:25 -0500

    theme context set up

---
+++ src/theme/themes.ts
@@ -0,0 +1,28 @@
+const themes: any = {
+  Light: {
+    primary: '#ffffff',
+    secondary: '#000000',
+    background: '#eeeeee',
+    text: '#333333',
+    header: 'yellow',
+  },
+  Dark: {
+    primary: '#333333',
+    secondary: '#dddddd',
+    background: '#222222',
+    text: '#ffffff',
+    header: 'gray',
+  },
+  Metal: {},
+  Psych: {},
+  Pop: {
+    primary: '#ff4081',
+    secondary: '#7c4dff',
+    background: '#cddc39',
+    text: '#212121',
+    header: 'pink',
+  },
+  Twee: {},
+};
+
+export default themes;
