3ca5b3b6f10d23e2760a46866175486234600262
Sterling Muller <sterlingmuller93@gmail.com>
2024-04-30 23:11:16 -0500

    fixed constant name in SongScreen

--- src/screens/SongScreen.tsx
+++ src/screens/SongScreen.tsx
@@ -7,7 +7,7 @@ import global from '@src/styles/global';
 import songScreenStyle from '@src/styles/songScreen';
 import DeleteModal from '@src/common/components/DeleteModal';
 import NotesModal from '@src/songFolder/components/NotesModal';
-import { DELETE_TAKE_TEXT, emptyTake } from '@src/common/constants';
+import { DELETE_TAKE_TEXT, EMPTY_TAKE } from '@src/common/constants';
 import { ScrollView } from 'react-native-gesture-handler';
 import { RootStackParamList, take } from '@src/common/types';
 import { RouteProp } from '@react-navigation/native';
@@ -19,7 +19,7 @@ interface Props {
 const SongScreen = ({ route }: Props) => {
   const { song } = route.params;
 
-  const [currentTake, setCurrentTake] = useState<take>(emptyTake);
+  const [currentTake, setCurrentTake] = useState<take>(EMPTY_TAKE);
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
   const [isNotesModalOpen, setIsNotesModalOpen] = useState<boolean>(false);
