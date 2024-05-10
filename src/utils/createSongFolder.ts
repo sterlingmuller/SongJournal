9eb25f41d29fdc0a519a4d8f88d723dbee84ad5c
Sterling Muller <sterlingmuller93@gmail.com>
2024-05-10 15:26:51 -0500

    setup file creators

---
+++ src/utils/createSongFolder.ts
@@ -0,0 +1,20 @@
+import * as FileSystem from 'expo-file-system';
+
+import { documentsDirectory } from '@src/common/constants';
+
+const newSongFolderPath = documentsDirectory + 'New Song';
+
+const createSongFolder = async () => {
+  const folderInfo = await FileSystem.getInfoAsync(newSongFolderPath);
+
+  if (!folderInfo.exists) {
+    await FileSystem.makeDirectoryAsync(newSongFolderPath, {
+      intermediates: true,
+    });
+    console.log('Folder created:', newSongFolderPath);
+  } else {
+    console.log('Folder already exists:', newSongFolderPath);
+  }
+};
+
+export default createSongFolder;
