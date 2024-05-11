c78e2963107d149be83512e7e74151064d5fcc70
Sterling Muller <sterlingmuller93@gmail.com>
2024-05-10 19:59:39 -0500

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
 
 import AppNavigator from '@src/navigation/AppNavigator';
 import { ColorThemeProvider } from '@src/theme/ThemeContext';
+import store from '@src/store/index';
 
 const App = () => (
   <GestureHandlerRootView style={{ flex: 1 }}>
     <ColorThemeProvider>
-      <AppNavigator />
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
     </ColorThemeProvider>
   </GestureHandlerRootView>
 );
