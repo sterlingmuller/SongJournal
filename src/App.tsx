26c4e526906889b2f574c85d4854bb8ab8615216
Sterling Muller <sterlingmuller93@gmail.com>
2024-05-14 00:33:08 -0500

    set up sqlite db

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 
 import AppNavigator from '@src/navigation/AppNavigator';
 import { ColorThemeProvider } from '@src/theme/ThemeContext';
 import { store } from '@src/store/index';
+import initDatabase from '@src/database/db';
 
-const App = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <ColorThemeProvider>
-      <Provider store={store}>
-        <AppNavigator />
-      </Provider>
-    </ColorThemeProvider>
-  </GestureHandlerRootView>
-);
+const App = () => {
+  useEffect(() => {
+    initDatabase()
+      .then(() => {
+        console.log('db is a goin');
+      })
+      .catch((err) => {
+        console.error("didn't work: ", err);
+      });
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ColorThemeProvider>
+        <Provider store={store}>
+          <AppNavigator />
+        </Provider>
+      </ColorThemeProvider>
+    </GestureHandlerRootView>
+  );
+};
 
 export default App;
