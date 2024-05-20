a38bd43b32ef071f94e87a0c27a81796934f9d34
Sterling Muller <sterlingmuller93@gmail.com>
2024-05-20 16:43:55 -0500

    set up audio recording

--- src/screens/RecordingScreen.tsx
+++ src/screens/RecordingScreen.tsx
@@ -1,17 +1,44 @@
-import StyledText from '@src/common/components/StyledText';
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
+import StyledText from '@src/common/components/StyledText';
 import RecordButton from '@src/common/components/RecordButton';
 import useGlobalStyles from '@src/styles/global';
+import {
+  playRecording,
+  startRecording,
+  stopRecording,
+} from '@src/utils/startStopPlayRecording';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const RecordingScreen = () => {
   const globalStyles = useGlobalStyles();
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [isRecording, setIsRecording] = useState<boolean>(true);
+  const [recordingUri, setRecordingUri] = useState<string | null>(null);
+
+  useEffect(() => {
+    startRecording(setRecording);
+  }, []);
+
+  const onRecordPress = () => {
+    isRecording
+      ? stopRecording(recording, setRecording, setRecordingUri)
+      : startRecording(setRecording);
+
+    setIsRecording(!isRecording);
+  };
 
   return (
     <View style={globalStyles.container}>
       <StyledText>Recording Screen hrm</StyledText>
-      <RecordButton onPress={() => console.log('Record pressed')} />
+      {recordingUri && (
+        <TouchableOpacity onPress={() => playRecording(recordingUri)}>
+          <Text>Press here to play sound</Text>
+        </TouchableOpacity>
+      )}
+      <RecordButton onPress={onRecordPress} isRecording={isRecording} />
     </View>
   );
 };
