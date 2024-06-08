871afe642ecbcd1d5dad8de0fd7292971601a02b
Sterling Muller <sterlingmuller93@gmail.com>
2024-06-07 19:04:28 -0500

    fixed sort by arrow icons

--- src/home/components/SongFolders.tsx
+++ src/home/components/SongFolders.tsx
@@ -4,16 +4,17 @@ import { SwipeListView } from 'react-native-swipe-list-view';
 import SongFolder from '@src/home/subcomponents/SongFolder';
 import DeleteRow from '@src/home/subcomponents/DeleteRow';
 import { ListRenderItemInfo } from 'react-native';
-import { deleteObject, song } from '@src/common/types';
+import { deleteObject, song, sortByCategoryName } from '@src/common/types';
 import { useAppSelector } from '@src/common/hooks';
 import { selectSongs } from '@src/selectors/songsSelector';
 import useAudioPlayer from '@src/utils/useAudioPlayer';
 
 interface Props {
   setToDelete: (value: deleteObject | null) => void;
+  sortedCategory: sortByCategoryName;
 }
 
-const SongFolders = ({ setToDelete }: Props) => {
+const SongFolders = ({ setToDelete, sortedCategory }: Props) => {
   const { togglePlayback } = useAudioPlayer();
   const songs = useAppSelector(selectSongs);
 
