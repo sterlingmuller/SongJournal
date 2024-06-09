6458643161368d7fdd9125e18fe0aeefea9a9934
Sterling Muller <sterlingmuller93@gmail.com>
2024-06-09 00:07:25 -0500

    added lyrics completed filters to sortSongs helper

--- src/utils/sortSongs.ts
+++ src/utils/sortSongs.ts
@@ -1,13 +1,45 @@
-import { song, sortByCategoryName, take } from '@src/common/types';
+import {
+  FilterOptions,
+  song,
+  sortByCategoryName,
+  take,
+} from '@src/common/types';
 
-// Improve this to filter songs by completion status and lyrics status
+const filterSongs = (songs: song[], filterOptions: FilterOptions) =>
+  songs.filter((song: song) => {
+    for (const filterKey in filterOptions) {
+      const filterValue = filterOptions[filterKey];
+
+      if (filterValue !== undefined) {
+        switch (filterKey) {
+          case 'Lyrics':
+            if (filterValue === true && !song.page?.lyrics.trim()) {
+              return false;
+            } else if (filterValue === false && song.page?.lyrics.trim()) {
+              return false;
+            }
+            break;
+          case 'Completed':
+            if (filterValue === true && !song.page?.completed) {
+              return false;
+            } else if (filterValue === false && song.page?.completed) {
+              return false;
+            }
+            break;
+        }
+      }
+    }
+    return true;
+  });
 
 export const sortSongs = (
   songs: song[],
   sortedCategory: sortByCategoryName,
   isSortAscending: boolean = true,
+  filterOptions: FilterOptions,
 ): song[] => {
-  const sortedSongs = [...songs];
+  const filteredSongs = filterSongs(songs, filterOptions);
+  const sortedSongs = [...filteredSongs];
 
   sortedSongs.sort((a: song, b: song) => {
     switch (sortedCategory) {
@@ -26,16 +58,18 @@ export const sortSongs = (
         return isSortAscending
           ? a.title.localeCompare(b.title)
           : b.title.localeCompare(a.title);
-      case 'Length': {
-        const durationA =
-          a.takes.find((take: take) => take.takeId === a.selectedTakeId)
-            ?.duration || 0;
-        const durationB =
-          b.takes.find((take: take) => take.takeId === b.selectedTakeId)
-            ?.duration || 0;
-        return isSortAscending ? durationA - durationB : durationB - durationA;
-      }
-      case 'Lyrics':
+      case 'Length':
+        {
+          const durationA =
+            a.takes.find((take: take) => take.takeId === a.selectedTakeId)
+              ?.duration || 0;
+          const durationB =
+            b.takes.find((take: take) => take.takeId === b.selectedTakeId)
+              ?.duration || 0;
+          return isSortAscending
+            ? durationA - durationB
+            : durationB - durationA;
+        }
         return isSortAscending
           ? (a.page?.lyrics.length || 0) - (b.page?.lyrics.length || 0)
           : (b.page?.lyrics.length || 0) - (a.page?.lyrics.length || 0);
