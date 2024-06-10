43b32a4b066f5d650ac94cc55eef7d20f1807c93
Sterling Muller <sterlingmuller93@gmail.com>
2024-06-09 23:25:33 -0500

    helper functrion to format date

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -1,1 +1,16 @@
 export const TOGGLE_PLAYBACK = 'playback/togglePlayback';
+
+export const MONTH_NAMES = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
