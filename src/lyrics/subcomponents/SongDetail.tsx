53865c4304046fd12634d600b2ad29ac04d753e4
Sterling Muller <sterlingmuller93@gmail.com>
2024-06-25 21:55:42 -0500

    working on info selector

--- src/lyrics/subcomponents/SongDetail.tsx
+++ src/lyrics/subcomponents/SongDetail.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 import useSongDetailStyles from '@src/styles/songDetail';
 import { SongInfo } from '@src/common/types';
+import { KEY_SIGNATURES } from '@src/common/enums';
 
 interface Props {
   label: string;
@@ -15,8 +17,35 @@ const SongDetail = (props: Props) => {
   const { label, value, onPageScreen, handleInputChange } = props;
   const styles = useSongDetailStyles();
 
-  return (
-    <View style={onPageScreen ? styles.pageContainer : styles.container}>
+  const isKey = label === 'Key';
+  const isTime = label === 'Time';
+
+  console.log('isKey:', label);
+
+  const renderInput =
+    isKey || isTime ? (
+      <RNPickerSelect
+        value={value}
+        onValueChange={(text: string) =>
+          handleInputChange(label as keyof SongInfo, text)
+        }
+        items={KEY_SIGNATURES}
+        style={{
+          inputAndroid: {
+            fontSize: 16,
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+            borderWidth: 0.5,
+            borderColor: 'purple',
+            borderRadius: 8,
+            color: 'black',
+            paddingRight: 30,
+          },
+        }}
+        placeholder={'wee'}
+        // ... your styles for the RNPickerSelect component
+      />
+    ) : (
       <TextInput
         style={onPageScreen ? styles.pageTextbox : styles.textbox}
         value={value}
@@ -25,6 +54,11 @@ const SongDetail = (props: Props) => {
         }
         textAlign="center"
       />
+    );
+
+  return (
+    <View style={onPageScreen ? styles.pageContainer : styles.container}>
+      {renderInput}
       <Text>{label}</Text>
     </View>
   );
