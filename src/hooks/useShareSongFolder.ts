df455c7837eb0a615c6aedda49fe8badf8215b5c
Sterling Muller <sterlingmuller93@gmail.com>
2024-07-09 17:02:36 -0500

    working on share hook

--- src/hooks/useShareSongFolder.ts
+++ src/hooks/useShareSongFolder.ts
@@ -2,24 +2,29 @@ import { useState, useCallback } from 'react';
 import * as FileSystem from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
 import { zip } from 'react-native-zip-archive';
-import { song } from '@src/common/types';
+import { song, take } from '@src/common/types';
 
 function useShareSongFolder() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const shareSongFolder = useCallback(async (song: song, lyrics: string) => {
+  const shareSongFolder = useCallback(async (song: song) => {
     setIsLoading(true);
     setError(null);
 
     try {
+      // await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+
       const folderName = song.title;
       const tempDir = FileSystem.cacheDirectory + folderName;
-      await FileSystem.makeDirectoryAsync(tempDir);
+      const zipDir = `${FileSystem.cacheDirectory}/${folderName}.zip`;
+      console.log('temp dir:', tempDir);
+      // await FileSystem.makeDirectoryAsync(tempDir);
 
       const starredTake = song.takes.find(
-        (take) => take.takeId === song.selectedTakeId,
+        (take: take) => take.takeId === song.selectedTakeId,
       );
+
       if (starredTake) {
         await FileSystem.copyAsync({
           from: starredTake.uri,
@@ -27,17 +32,30 @@ function useShareSongFolder() {
         });
       }
 
-      await FileSystem.writeAsStringAsync(`${tempDir}/lyrics.txt`, lyrics);
+      // if (song.page.lyrics) {
+      //   await FileSystem.writeAsStringAsync(
+      //     `${tempDir}/lyrics.txt`,
+      //     song.page.lyrics,
+      //   );
+      // }
 
-      // const zipUri = await zipDirectoryAsync(tempDir, {
-      //   name: `${folderName}.zip`,
-      // });
+      console.log('hi');
+      const contents = await FileSystem.readDirectoryAsync(
+        `${FileSystem.cacheDirectory}/La`,
+      ); // Get the list of files and directories
+
+      console.log('Cache directory contents:', contents);
+
+      const zipUri = await zip(tempDir, zipDir);
+
+      console.log('hi ty');
 
       await Sharing.shareAsync(zipUri, {
         mimeType: 'application/zip',
         dialogTitle: `Share ${folderName}`,
         UTI: 'public.zip-archive',
       });
+      // Sharing.shareAsync(tempDir);
     } catch (err) {
       setError((err as Error).message); // Or handle error differently
     } finally {
