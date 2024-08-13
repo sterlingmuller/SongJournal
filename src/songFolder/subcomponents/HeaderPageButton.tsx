abe3e6502da1db4f4f6cd3f098e25968df3aff05
Sterling Muller <sterlingmuller93@gmail.com>
2024-08-13 12:41:55 -0500

    fetches page info when navigating from song screen, removed unused hooks

--- src/songFolder/subcomponents/HeaderPageButton.tsx
+++ src/songFolder/subcomponents/HeaderPageButton.tsx
@@ -5,12 +5,20 @@ import { NavigationProp, useNavigation } from '@react-navigation/native';
 import PageIcon from '@src/icons/PageIcon';
 import { RootStackParamList } from '@src/common/types';
 import { useAudioPlayer } from '@src/context/AudioContext';
+import { useAppDispatch, useAppSelector } from '@src/common/hooks';
+import { fetchPageRequest } from '@src/sagas/actionCreators';
+import { useSQLiteContext } from 'expo-sqlite';
+import { selectCurrentSongId } from '@src/selectors/songsSelector';
 
 const HeaderPageButton = () => {
   const { navigate } = useNavigation<NavigationProp<RootStackParamList>>();
   const { clearPlayback } = useAudioPlayer();
+  const dispatch = useAppDispatch();
+  const db = useSQLiteContext();
+  const songId = useAppSelector(selectCurrentSongId);
 
   const onNavigationPress = () => {
+    dispatch(fetchPageRequest({ songId, db }));
     clearPlayback();
 
     navigate('Lyrics');
