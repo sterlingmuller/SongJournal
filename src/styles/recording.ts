539058bb6012ec51a908a8aec2028fa2ad30be02
Sterling Muller <sterlingmuller93@gmail.com>
2024-08-14 23:38:28 -0500

    added timer to recording screen, refactored recording screen by adding RecordingControls component

--- src/styles/recording.ts
+++ src/styles/recording.ts
@@ -1,21 +1,52 @@
-import { StyleSheet, ViewStyle } from 'react-native';
+import { useColorTheme } from '@src/context/ThemeContext';
+import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 interface Styles {
+  container: ViewStyle;
   recordingRow: ViewStyle;
+  sideButton: ViewStyle;
+  buttonText: TextStyle;
+  recordButtonContainer: ViewStyle;
+  timerContainer: ViewStyle;
+  timer: TextStyle;
 }
 
 const useRecordingStyles = () => {
+  const { theme } = useColorTheme();
+
   const recordingStyles: Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: theme.primaryBackground,
+      bottom: 0,
+    },
     recordingRow: {
       flexDirection: 'row',
       position: 'absolute',
       flex: 5,
       alignItems: 'center',
-      alignSelf: 'center',
       justifyContent: 'space-around',
       width: '100%',
       bottom: 50,
     },
+    sideButton: {
+      width: 120,
+      alignItems: 'center',
+    },
+    buttonText: {
+      fontSize: 18,
+    },
+    recordButtonContainer: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    timerContainer: {
+      justifyContent: 'center',
+      alignSelf: 'center',
+      position: 'absolute',
+      bottom: 200,
+    },
+    timer: { fontSize: 82 },
   });
 
   return recordingStyles;
