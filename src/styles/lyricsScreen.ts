c446269cba42138cc6a73e66d5f87aa1ff10d8ea
Sterling Muller <sterlingmuller93@gmail.com>
2024-09-10 17:25:16 -0500

    fixed open keyboard styling on lyrics screen

--- src/styles/lyricsScreen.ts
+++ src/styles/lyricsScreen.ts
@@ -1,13 +1,11 @@
 import { StyleSheet, ViewStyle } from 'react-native';
 import { useColorTheme } from '@src/state/context/ThemeContext';
-import { SCREEN_HEIGHT } from '@src/components/common/constants';
 
 interface Styles {
   editTextContainer: ViewStyle;
   lyricsContainer: ViewStyle;
   container: ViewStyle;
   details: ViewStyle;
-  keyboardAvoidingViewContainer: ViewStyle;
 }
 
 const useLyricScreenStyles = () => {
@@ -19,19 +17,12 @@ const useLyricScreenStyles = () => {
       backgroundColor: theme.primaryBackground,
     },
 
-    keyboardAvoidingViewContainer: {
-      // flex: 1,
-      width: '80%',
-      height: SCREEN_HEIGHT * 0.5,
-      alignSelf: 'center',
-    },
-
     editTextContainer: {
+      width: '90%',
+      alignSelf: 'center',
       backgroundColor: '#fff',
       borderColor: 'black',
       borderWidth: 1,
-      width: '100%',
-      height: SCREEN_HEIGHT * 0.5,
       borderRadius: 15,
       paddingHorizontal: 20,
       paddingVertical: 10,
