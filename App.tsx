2ac023b6ecbe165a3bf28191c2e133e13db8a321
Sterling Muller <sterlingmuller93@gmail.com>
2024-09-18 16:04:30 -0500

    notification bar now transparent

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { SQLiteProvider } from 'expo-sqlite';
+import { StatusBar } from 'react-native';
 
 import AppNavigator from '@src/navigation/AppNavigator';
 import { ColorThemeProvider } from '@src/state/context/ThemeContext';
@@ -12,6 +13,7 @@ import { dbName } from '@src/components/common/constants';
 
 const App = () => (
   <GestureHandlerRootView style={{ flex: 1 }}>
+    <StatusBar translucent backgroundColor="transparent" />
     <ColorThemeProvider>
       <Suspense fallback={null}>
         <SQLiteProvider
