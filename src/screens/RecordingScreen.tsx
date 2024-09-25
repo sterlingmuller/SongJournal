1e07fa410e3c59a05ba3b1965def2772bc0d853d
Sterling Muller <sterlingmuller93@gmail.com>
2024-09-25 18:03:14 -0500

    reworking wave display

--- src/screens/RecordingScreen.tsx
+++ src/screens/RecordingScreen.tsx
@@ -22,7 +22,14 @@ const RecordingScreen = () => {
     ...EMPTY_AUDIO_WAVE_ARRAY,
   ]);
 
-  const [fullWave, setFullWave] = useState<number[]>([...LEADING_DOTS_ARRAY]);
+  // const [fullWave, setFullWave] = useState<number[]>([...LEADING_DOTS_ARRAY]);
+  const MOCK_FULL_WAVE = [
+    75, 75, 74, 74, 73, 76, 65, 67, 66, 77, 76, 73, 78, 83, 76, 84, 86, 74, 56,
+    65, 67, 75, 73, 87, 76, 78, 65, 65, 67, 67, 76, 75, 73, 74, 74, 73, 72, 75,
+    75, 83, 82, 81, 80, 78, 75, 79, 77, 76, 77, 77,
+  ];
+
+  const [fullWave, setFullWave] = useState<number[]>([]);
   const scrollPositionRef = useRef<number>(0);
 
   const isPlaying = useAppSelector(selectIsPlaying);
