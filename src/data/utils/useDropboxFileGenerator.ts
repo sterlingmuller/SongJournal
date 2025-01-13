b91422bf5da284a757ba0d18a6a7509cbafa3a2e
Sterling Muller <sterlingmuller93@gmail.com>
2025-01-13 17:36:14 -0600

    working on batching dropbox requests, getting not_closed error

--- src/data/utils/useDropboxFileGenerator.ts
+++ src/data/utils/useDropboxFileGenerator.ts
@@ -2,7 +2,10 @@ import { useCallback, useEffect, useState } from 'react';
 import * as FileSystem from 'expo-file-system';
 import { Buffer } from 'buffer';
 
-import { backupSong } from '@src/data/utils/uploadToDropbox';
+import {
+  getValidAccessToken,
+  uploadFilesInBatch,
+} from '@src/data/utils/uploadToDropbox';
 import { useAppSelector } from '@src/utils/hooks/typedReduxHooks';
 import { selectSongs } from '@src/state/selectors/songsSelector';
 import { Page, Song, SongToPageMap, Take } from '@src/components/common/types';
@@ -15,6 +18,7 @@ const useDropboxSongFolderGenerator = () => {
   const db = useSQLiteContext();
   const songs = useAppSelector(selectSongs);
   const [pages, setPages] = useState<SongToPageMap>({});
+  const [trigger, setTrigger] = useState(false);
 
   const { getArtistName } = useArtistName();
 
@@ -23,6 +27,66 @@ const useDropboxSongFolderGenerator = () => {
     const pdfUri = await generatePagePdf(title, page, artist);
 
     return pdfUri;
+  };
+
+  const doesFolderExists = async (folderPath: string, accessToken: string) => {
+    try {
+      const response = await fetch(
+        'https://api.dropboxapi.com/2/files/get_metadata',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ path: folderPath }),
+        },
+      );
+
+      if (response.ok) {
+        return true;
+      } else {
+        const data = await response.json();
+        if (data.error_summary.includes('path/not_found')) {
+          return false;
+        } else {
+          console.error('Error checking folder existence:', data);
+          return false;
+        }
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+      return false;
+    }
+  };
+
+  const createDropboxFolder = async (folderPath: string) => {
+    const accessToken = await getValidAccessToken();
+
+    if (await doesFolderExists(folderPath, accessToken)) {
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        'https://api.dropboxapi.com/2/files/create_folder_v2',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ path: folderPath, autorename: false }),
+        },
+      );
+
+      if (!response.ok) {
+        const data = await response.json();
+        console.error('Error creating folder:', data);
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+    }
   };
 
   useEffect(() => {
@@ -34,7 +98,7 @@ const useDropboxSongFolderGenerator = () => {
     fetchPagesData();
   }, [db]);
 
-  const generateAndBackupSong = async (song: Song, page: Page) => {
+  const generateSongBuffers = async (song: Song, page: Page) => {
     const { title, selectedTakeId, takes, completed } = song;
 
     let lyricsBuffer: Buffer | undefined;
@@ -78,19 +142,103 @@ const useDropboxSongFolderGenerator = () => {
       }
     }
 
-    if (lyricsBuffer || selectedTakeBuffer || takesBuffers.length > 0) {
-      await backupSong(title, lyricsBuffer, selectedTakeBuffer, takesBuffers);
-    }
+    return { title, lyricsBuffer, selectedTakeBuffer, takesBuffers };
   };
 
+  useEffect(() => {
+    if (!trigger) return;
+
+    const performBackup = async () => {
+      const accessToken = await getValidAccessToken();
+      const filesToUpload = [];
+
+      for (const song of songs) {
+        await createDropboxFolder(`/${song.title}`);
+
+        const page = pages[song.songId];
+        const { title, lyricsBuffer, selectedTakeBuffer, takesBuffers } =
+          await generateSongBuffers(song, page);
+
+        if (lyricsBuffer) {
+          filesToUpload.push({
+            path: `/${title}/Lyrics.pdf`,
+            content: lyricsBuffer,
+          });
+        }
+
+        if (selectedTakeBuffer) {
+          filesToUpload.push({
+            path: `/${title}/${title}.mp3`,
+            content: selectedTakeBuffer,
+          });
+        }
+
+        if (takesBuffers.length > 0) {
+          await createDropboxFolder(`/${song.title}/Takes`);
+          for (const take of takesBuffers) {
+            filesToUpload.push({
+              path: `/${title}/Takes/${take.title}.mp3`,
+              content: take.takeBuffer,
+            });
+          }
+        }
+      }
+
+      await uploadFilesInBatch(filesToUpload, accessToken);
+      setTrigger(false);
+    };
+
+    performBackup();
+  }, [trigger, songs, pages]);
+
   const triggerBackup = useCallback(() => {
-    songs.forEach((song: Song) => {
-      const page = pages[song.songId];
-      generateAndBackupSong(song, page);
-    });
-  }, [songs, pages]);
+    setTrigger(true);
+  }, []);
 
   return triggerBackup;
 };
 
 export default useDropboxSongFolderGenerator;
+
+// export const uploadFilesInBatch = async (
+//   files: { path: string; content: Buffer }[],
+//   accessToken: string,
+// ) => {
+//   const entries = [];
+
+//   console.log('uploading files in batch:', files);
+
+//   for (const file of files) {
+//     const sessionId = await startUploadSession(file.content, accessToken);
+//     const EIGHT_MB_CHUNK = 8 * 1024 * 1024;
+//     let offset = 0;
+
+//     while (offset < file.content.length) {
+//       const chunk = file.content.subarray(offset, offset + EIGHT_MB_CHUNK);
+//       const isLastChunk = offset + EIGHT_MB_CHUNK >= file.content.length;
+//       await appendToUploadSession(
+//         sessionId,
+//         chunk,
+//         accessToken,
+//         offset,
+//         isLastChunk,
+//       );
+//       offset += EIGHT_MB_CHUNK;
+//     }
+
+//     entries.push({
+//       cursor: { session_id: sessionId, offset: file.content.length },
+//       commit: {
+//         path: `${file.path}`,
+//         mode: 'add',
+//         autorename: true,
+//         mute: false,
+//       },
+//     });
+//   }
+
+//   const result = await finishUploadSessionBatch(entries, accessToken);
+//   if (result['.tag'] === 'async_job_id') {
+//     await checkUploadSessionBatchStatus(result.async_job_id, accessToken);
+//   }
+// };
