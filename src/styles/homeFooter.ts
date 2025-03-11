d324a4610b938d1961e9e17fb41d32e6649e405a
Sterling Muller <sterlingmuller93@gmail.com>
2025-03-11 17:56:14 -0500

    cleaned up footer styling

--- src/styles/homeFooter.ts
+++ src/styles/homeFooter.ts
@@ -12,7 +12,7 @@ const useHomeFooterStyles = () => {
     container: {
       flexDirection: 'row',
       width: '100%',
-      height: 80,
+      height: '9%',
       alignItems: 'center',
       justifyContent: 'space-around',
       backgroundColor: theme.primary,
