db15c63cb6713eac04958e5830918eb2894127af
Sterling Muller <sterlingmuller93@gmail.com>
2025-03-18 18:36:24 -0500

    cleaned up keyboard view on lyrics editor

--- src/components/lyrics/components/EditLyricsSheet.tsx
+++ src/components/lyrics/components/EditLyricsSheet.tsx
@@ -1,10 +1,9 @@
 import React, { useRef } from 'react';
-import { View } from 'react-native';
+import { KeyboardAvoidingView, Platform, View } from 'react-native';
 
 import useLyricScreenStyles from '@src/styles/lyricsScreen';
 import TextEditor from '@src/components/common/components/TextEditor';
 import useDebounce from '@src/hooks/useDebounce';
-import useKeyboardHeight from '@src/hooks/useKeyboardHeight';
 import { SCREEN_HEIGHT } from '@src/components/common/constants';
 
 interface Props {
@@ -15,8 +14,6 @@ interface Props {
 const EditLyricsSheet = ({ newLyrics, setNewLyrics }: Props) => {
   const styles = useLyricScreenStyles();
   const initialLyricsRef = useRef(newLyrics);
-  const keyboardHeight = useKeyboardHeight();
-  const textBoxHeight = SCREEN_HEIGHT * 0.8 - keyboardHeight;
 
   const debouncedLyrics = useDebounce((lyrics: string) => {
     setNewLyrics(lyrics);
@@ -27,12 +24,17 @@ const EditLyricsSheet = ({ newLyrics, setNewLyrics }: Props) => {
   };
 
   return (
-    <View style={[styles.editTextContainer, { height: textBoxHeight }]}>
-      <TextEditor
-        initialText={initialLyricsRef.current}
-        setText={handleLyricsChange}
-      />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={SCREEN_HEIGHT * 0.05}
+    >
+      <View style={[styles.editTextContainer, { height: '90%' }]}>
+        <TextEditor
+          initialText={initialLyricsRef.current}
+          setText={handleLyricsChange}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
