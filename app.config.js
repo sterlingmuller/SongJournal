1e7a7be365ede994d5300edb8732991946c1e04f
Sterling Muller <sterlingmuller93@gmail.com>
2025-04-06 19:50:15 -0500

    updated version

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default ({ config }) => {
     name: IS_DEV ? 'SJ (Dev)' : 'Song Journal',
     slug: 'SongJournal',
     scheme: IS_DEV ? 'songjournaldev' : 'songjournal',
-    version: '0.2.5',
+    version: '0.2.6',
     orientation: 'portrait',
     icon: './assets/Icon.png',
     userInterfaceStyle: 'light',
@@ -105,7 +105,7 @@ export default ({ config }) => {
     },
     android: {
       softwareKeyboardLayoutMode: 'pan',
-      versionCode: 16,
+      versionCode: 17,
       package: IS_DEV
         ? 'com.sterling.silverado.songjournal.dev'
         : 'com.sterling.silverado.songjournal',
@@ -126,7 +126,7 @@ export default ({ config }) => {
       DROPBOX_CLIENT_SECRET: process.env.DROPBOX_CLIENT_SECRET,
     },
     owner: 'sterlo',
-    runtimeVersion: '0.2.5',
+    runtimeVersion: '0.2.6',
     updates: {
       url: 'https://u.expo.dev/782ab462-3c4f-4302-ae95-e24dfe286e87',
     },
